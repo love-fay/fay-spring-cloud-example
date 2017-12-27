b965cd136340724d724c16358f28eaca34663b68
fay <869770665@qq.com>
2017-12-27 17:13:23 +0800

    api gateway

---
+++ api-gateway/src/main/java/com/fay/AccessFilter.java
@@ -0,0 +1,48 @@
+package com.fay;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class AccessFilter extends ZuulFilter {
+	
+	private static Logger log = LoggerFactory.getLogger(AccessFilter.class);
+
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		
+		log.info("send {} request to {}", request.getMethod(), request.getRequestURL().toString());
+		
+		Object accessToken = request.getParameter("accessToken");
+		if(accessToken == null) {
+			log.warn("access token is empty");
+			ctx.setSendZuulResponse(false);
+			ctx.setResponseStatusCode(401);
+			return null;
+		}
+		log.info("access token ok");
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+}
